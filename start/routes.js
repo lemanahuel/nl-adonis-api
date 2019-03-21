85e6ec445cd735ef7f84f60807bd23ea7562dc22
lemanahuel <lema.nahuel@gmail.com>
2019-03-21 20:34:50 -0300

    IMPROVE, adonis

--- start/routes.js
+++ start/routes.js
@@ -2,6 +2,14 @@
 
 const Route = use('Route');
 
-Route.on('/').render('welcome')
+Route.on('/').render('welcome');
+Route.get('/docs', ({ view }) => {
+  return view.render('docs', {
+    app: {
+      title: 'Adonisjs Docs'
+    },
+    endpoints: []
+  });
+});
 
 require('../app/Routes/TasksRoutes')(Route);
