fbe2dab44f2e3bfb1d4d1dea7702b0615e108951
lemanahuel <lema.nahuel@gmail.com>
2019-03-23 07:13:03 -0300

    IMPROVE, docs

--- start/routes.js
+++ start/routes.js
@@ -2,13 +2,22 @@
 
 const Route = use('Route');
 
-Route.on('/').render('welcome');
+// Route.on('/').render('welcome');
+Route.get('/', ({ response }) => {
+  return response.redirect('docs', false, 301);
+});
+
 Route.get('/docs', ({ view }) => {
   return view.render('docs', {
     app: {
       title: 'Adonisjs Docs'
     },
-    endpoints: []
+    endpoints: Route.list().map(endpoint => {
+      return {
+        path: endpoint.name,
+        methods: endpoint.verbs
+      };
+    })
   });
 });
 
